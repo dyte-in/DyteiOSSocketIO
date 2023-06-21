9c15fc03e2eee34620d15bedc48312c7f6ab0a01
Shaunak Jagtap <shaunakjagtap@gmail.com>
2023-06-21 18:28:41 +0530

    release: spm fix

--- Package.swift
+++ Package.swift
@@ -18,18 +18,8 @@ let package = Package(
     targets: [
         .target(
             name: "DyteiOSSocketIO",
-            dependencies: ["SocketIO"],
-            path: "Sources"
-        ),
-        .target(
-            name: "SocketIO",
-            dependencies: [],
-            path: "Source/SocketIO" // Adjust the path to your SocketIO target
-        ),
-        .testTarget(
-            name: "DyteiOSSocketIOTests",
-            dependencies: ["DyteiOSSocketIO"],
-            path: "Tests"
+            dependencies: ["Starscream"],
+            path: "Source/SocketIO"
         )
     ]
 )

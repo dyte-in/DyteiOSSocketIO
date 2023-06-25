b08542d7a477c331c6d0278d80516a3466cdfca1
Shaunak Jagtap <shaunakjagtap@gmail.com>
2023-06-25 23:52:43 +0530

    fix: updated targets

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "DyteiOSSocketIO",
-            targets: ["DyteiOSSocketIO"]
+            targets: ["DyteSocketIO"]
         )
     ],
     targets: [

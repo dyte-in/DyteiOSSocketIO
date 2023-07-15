33ca55250385a77d83583d1cbf233fc6bac4578e
Shaunak Jagtap <shaunakjagtap@gmail.com>
2023-07-15 17:25:47 +0530

    fix: added socketio stable release

--- Package.swift
+++ Package.swift
@@ -1,20 +1,26 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "DyteiOSSocketIO",
-    platforms: [
-        .iOS(.v13)
-    ],
+    platforms: [.iOS(.v13)],
     products: [
-        .library(
-            name: "DyteiOSSocketIO",
-            targets: ["DyteSocketIO"]
-        )
+        .library(name: "DyteiOSSocketIO", targets: ["DyteiOSSocketIOTargets"]),
     ],
+    dependencies: [.package(url: "https://github.com/dyte-in/DyteStarscream.git", from: "0.0.2")],
     targets: [
-        .binaryTarget(name: "DyteiOSSocketIO", path: "Source/DyteiOSSocketIO.xcframework.zip"),
-        .binaryTarget(name: "Starscream", path: "Source/Starscream.xcframework.zip"),
-        .target(name: "DyteSocketIO", dependencies: ["Starscream", "DyteiOSSocketIO"], path: "Source/")
+        .binaryTarget(
+            name: "DyteiOSSocketIO",
+            url: "https://github.com/dyte-in/DyteiOSSocketIO/archive/refs/tags/0.1.3.zip",
+            checksum: "ca0ffe1930f5d5f472abb4966b2b814489b463399fb7aa757b2ab8bd3570048e"
+        ),
+        .target(
+            name: "DyteiOSSocketIOTargets",
+            dependencies: [
+                .target(name: "DyteiOSSocketIO"),
+                "DyteStarscream"
+            ],
+            path: "Sources/Wrapper"
+        )
     ]
 )
